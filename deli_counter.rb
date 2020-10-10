5da8a4d049d7b67076ceda4f89e4d63a0926655f
Cara Zellner <carajeanine@gmail.com>
2020-10-10 23:19:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length >= 1 
-    puts "The line is currently: #{katz_deli.index}."
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   end
 end
