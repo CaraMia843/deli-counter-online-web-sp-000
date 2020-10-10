42ee88d877dbff8453b372c55e287f2d28116661
Cara Zellner <carajeanine@gmail.com>
2020-10-10 23:41:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length >= 1
     new_katz_deli = []
-    counter = 0
+    counter = 1
     katz_deli.each do |name|
-      new_katz_deli.push("#{counter + 1}. #{name}")
+      new_katz_deli.push("#{counter}. #{name}")
       counter =+ 1 
     end
     puts "The line is currently: #{new_katz_deli.join(" ")}"
