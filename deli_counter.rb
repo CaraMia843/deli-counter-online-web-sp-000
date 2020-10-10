9a40c84665b498bc26f41bec537326036c14598d
Cara Zellner <carajeanine@gmail.com>
2020-10-10 23:30:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,5 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0 
-    puts "The line is currently empty."
-  elsif katz_deli.length >= 1 
-    new_array = []
-    counter = 1 
-    katz_deli.each do |name|
-      new_array.push("#{counter}. #{name}")
-      counter += 1 
-  else
-    puts "The line is currently: #{new_array.join(" ")}"
-  end
+  
 end
