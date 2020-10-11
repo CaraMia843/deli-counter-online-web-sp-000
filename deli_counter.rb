922e83d6b1be2f5e3a4bdbd4839e7634ae57d767
Cara Zellner <carajeanine@gmail.com>
2020-10-11 00:02:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,4 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  end
 end
-katz_deli.empty ? "There is nobody waiting to be served!" : "Currently serving #{katz_deli.first}"
